8ec92ef8d261a58f27b106d89f84d8d01ab5fd79
nisan112 <111062770+nisan112@users.noreply.github.com>
2024-05-21 23:29:43 -0400

    Update firstClass.java

--- firstClass.java
+++ firstClass.java
@@ -1,11 +1,1 @@
-package myProjectFormyRepo;
 
-public class firstClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-
-	}
-
-}
